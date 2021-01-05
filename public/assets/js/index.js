13108b7faf20946d748ad5afdb70b1812d7370d9
Luke Martin <ljmart02@gmail.com>
2021-01-04 20:47:35 -0700

    Routes added

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,11 +3,31 @@ const $noteText = $(".note-textarea");
 const $saveNoteBtn = $(".save-note");
 const $newNoteBtn = $(".new-note");
 const $noteList = $(".list-container .list-group");
+var express = require("express");
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+var path = require("path")
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
+
+
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Basic route that sends the userto the notes page
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
 
 // A function for getting all notes from the db
 const getNotes = () => {
@@ -151,3 +171,7 @@ $noteText.on("keyup", handleRenderSaveBtn);
 
 // Gets and renders the initial list of notes
 getAndRenderNotes();
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
