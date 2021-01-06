7d6c051f49aa183d579718b2beda42c8b313e15d
Luke Martin <ljmart02@gmail.com>
2021-01-05 17:27:49 -0700

    Start added in JSON

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -17,9 +17,8 @@ app.use(express.json());
 let activeNote = {};
 
 
-
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
+app.use( function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
@@ -28,6 +27,10 @@ app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "notes.html"));
 });
 
+// When you go to the notes page, display the data in the notes array
+app.get("/notes", function(req, res) {
+  res.json(newNote);
+});
 
 // A function for getting all notes from the db
 const getNotes = () => {
